48f597de5c388e16e2d103f7322034b303cf0d13
cherkaoui mouad <mouad.cherkaoui@sanasoft.ma>
2021-03-24 21:41:57 +0100

    Update: loading app.xaml in the main entry point

--- src/Program.fs
+++ src/Program.fs
@@ -3,16 +3,14 @@ open System.Windows
 open FSharpWPF.ViewModels
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
 
 [<STAThread>]
 [<EntryPoint>]
-let main argv =
-    let mainWindow = 
-        Uri("/FSharpwPF;component/Views/MainWindow.xaml", UriKind.Relative)
-        |> fun uri -> Application.LoadComponent(uri) :?> Window
+let main argv =        
+    let application = 
+        Uri("/FSharpwPF;component/App.xaml", UriKind.Relative)
+        |> fun uri -> Application.LoadComponent(uri) :?> Application
 
-    mainWindow.ShowDialog() |> ignore
-    
+    application.Run() |> ignore
+
     0 // return an integer exit code
